4ae49eac96709d6a844791b4ed4aad68427675ef
manu pascual <mpascual1988@gmail.com>
2015-04-20 18:03:37 +0200

    pequeño cambio linped

--- Modelo/EN/LinpedEN.cs
+++ Modelo/EN/LinpedEN.cs
@@ -7,5 +7,15 @@ namespace Modelo.EN
 {
     public class LinpedEN
     {
+        private int id;
+        public int Id { get { return id; } set { id = value; } }
+
+        private IList<ProductoEN> productos;
+        public IList<ProductoEN> Productos { get { return productos; } set { productos = value; } }
+
+        public LinpedEN()
+        {
+
+        }
     }
 }
