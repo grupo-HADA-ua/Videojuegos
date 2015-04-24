b55e8c2ef7f14c48943fc2bc0c8d54b2fc0f55f8
manu pascual <mpascual1988@gmail.com>
2015-04-24 13:22:51 +0200

    añade contructores carrito

--- Modelo/EN/CarritoEN.cs
+++ Modelo/EN/CarritoEN.cs
@@ -7,5 +7,34 @@ namespace Modelo.EN
 {
     class CarritoEN
     {
+        private IList<LineaPedidoEN> lineas;
+        public IList<LineaPedidoEN> Lineas
+        {
+            get
+            {
+                return new List<LineaPedidoEN>(lineas);
+            }
+            set
+            {
+                lineas = new List<LineaPedidoEN>(value);
+            }
+        }
+
+        public CarritoEN()
+        { }
+
+        public CarritoEN(CarritoEN c)
+        {
+            inicializar(c.Lineas);
+        }
+
+        public CarritoEN(IList<LineaPedidoEN> l)
+        {
+            inicializar(l);
+        }
+
+        private void inicializar(IList<LineaPedidoEN> l){
+            Lineas = l;
+        }
     }
 }
