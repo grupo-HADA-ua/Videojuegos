e66dc5660d2c7c0fde834fd5c7d29dd26716ddf6
aor92 <alexor92@gmail.com>
2015-04-29 12:25:35 +0200

    solucionado problema en CADCliente y creacion de una nueva rama para los CAD

--- Modelo/CAD/ConsolaCAD.cs
+++ Modelo/CAD/ConsolaCAD.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using Modelo.EN;
 
 namespace Modelo.CAD
 {
